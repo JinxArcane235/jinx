cba57b15097e572dd3c5f596db4215ecb71fff42
shivaji <108468912+shivaji1134@users.noreply.github.com>
2024-11-27 19:18:44 +0530

    "fixed bug for rsi value"

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
 import puppeteer from 'puppeteer';
 
 async function main() {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: false ,defaultViewport: {
+        width: 1415,  
+        height: 816  
+    } });
     const page = await browser.newPage();
     await page.goto('https://www.tradingview.com/accounts/signin/');
     console.log('Waiting for manual login...');
     await new Promise(resolve => setTimeout(resolve, 60000));
     console.log('Proceeding to chart after 1 minute...');
-    await page.goto('https://www.tradingview.com/chart/hzNp15qZ/?symbol=COINBASE%3ASOLUSD');
+    await page.goto('https://www.tradingview.com/chart/hmLYX6Nq/?symbol=COINBASE%3ASOLUSD');
     await page.waitForSelector('.chart-container');
 
     const getSolPriceAndRSI = async () => {
         const data = await page.evaluate(() => {
             const priceElement = document.querySelector('span.highlight-maJ2WnzA.highlight-BSF4XTsE.price-qWcO4bp9') as HTMLElement;
-            const rsiElement = document.querySelector('div.valueValue-l31H9iuA.apply-common-tooltip') as HTMLElement;
+            const rsiElement = document.querySelector('div.valueValue-l31H9iuA.apply-common-tooltip:not([title="Volume"]):is([style*="rgb(255, 237, 71)"])') as HTMLElement;
             const price = priceElement ? priceElement.innerText : null;
             const rsi = rsiElement ? rsiElement.innerText : null;
             return { price, rsi };
